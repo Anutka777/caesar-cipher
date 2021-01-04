be68f0e64a595e3af1636a19fad7c056f686c7d8
Anna Baburina <rudak0wa.nat@yandex.ru>
2021-01-04 23:52:58 +0500

    Refactor, fix capital case letters overlaping

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -3,22 +3,31 @@ def caesar_cipher(string, key)
   string_array = string.split('')
   result_array = []
   string_array.each do |char|
-    if char =~ /[a-zA-Z]/
+    if char =~ /[a-z]/
       if (char.ord + key) > 122
-        char = (97 + (char.ord + key) - 123).chr
-      elsif (char.ord + key) > 90 and (char.ord + key) < 97
-        char = (65 + (char.ord + key) - 91).chr
+        new_char = (97 + char.ord + key - 123).chr
       else
-        char = (char.ord + key).chr
+        new_char = (char.ord + key).chr
       end
+    elsif char =~ /[A-Z]/
+      if (char.ord + key) > 90
+        new_char = (65 + char.ord + key - 91).chr
+      else
+        new_char = (char.ord + key).chr
+      end
+    else
+        new_char = char
     end
-    result_array.push(char)
+    result_array.push(new_char)
   end
-  result_array.join()
+  result_array.join('')
 end
 
 # p caesar_cipher("What a string!", 5)
-# p caesar_cipher("1  684 44", 4)
-# p caesar_cipher(">?@ - AB=CDE|FGH-56qIJ,,KL.MN//O+P;QRS'TUVW", 115)
-# p caesar_cipher("abcdefghijklmnopq", 503)
-# p caesar_cipher("abcdefghijklmnopq", 3)
+# p caesar_cipher("1  684 44 0", 4)
+# p caesar_cipher(">?@ - AB=CDE|FGH-56qIJ,,KL.M`N//O+P;QR~S'TUVW{_}XYZ", 1)
+# p caesar_cipher("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 23)
+# p caesar_cipher("abcdefghijklmnopqrstuvwxyz", 503)
+# p caesar_cipher("abcdefghijklmnopqrstuvwxyz", 3)
+# p caesar_cipher("Z", 7)
+# p caesar_cipher("z", 7)
