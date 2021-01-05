8654ee16b372ce56dadd9f961416d1180ea5f957
Anna Baburina <rudak0wa.nat@yandex.ru>
2021-01-05 16:44:22 +0500

    Fix for large keys

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,5 +1,5 @@
 def caesar_cipher(string, key)
-  key = key % 25
+  key = key % 26
   string_array = string.split('')
   result_array = []
   string_array.each do |char|
@@ -26,8 +26,8 @@ end
 # p caesar_cipher("What a string!", 5)
 # p caesar_cipher("1  684 44 0", 4)
 # p caesar_cipher(">?@ - AB=CDE|FGH-56qIJ,,KL.M`N//O+P;QR~S'TUVW{_}XYZ", 1)
-# p caesar_cipher("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 23)
-# p caesar_cipher("abcdefghijklmnopqrstuvwxyz", 503)
+# p caesar_cipher("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 25)
+# p caesar_cipher("abcdefghijklmnopqrstuvwxyz", 55)
 # p caesar_cipher("abcdefghijklmnopqrstuvwxyz", 3)
 # p caesar_cipher("Z", 7)
 # p caesar_cipher("z", 7)
